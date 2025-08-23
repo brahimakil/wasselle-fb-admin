e99fedd853b6d04153903998eca22020aa12200b
ibrahim akil <brhimakil.1@gmail.com>
2025-08-23 07:34:19 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'no-unused-vars': 'off'
+  }
+}
